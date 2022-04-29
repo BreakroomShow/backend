c3a9b0592dc12f0039c978cfab7d9f172db104d9
Seva Zhidkov <seva@zhidkoff.com>
2022-04-29 14:24:09 +0300

    show replay by concrete id

--- api/replays.py
+++ api/replays.py
@@ -9,7 +9,7 @@ router = APIRouter()
 
 @router.get('/replays/last')
 def get_last(redis_conn: redis_connection.Redis = Depends(redis_connection.get)):
-    last_replay = replay.get_last(redis_conn)
+    last_replay = replay.get_by_game_id(redis_conn, 'aaa4b02e-4f12-4482-97a6-692b606f9422')
     if not last_replay:
         raise HTTPException(status_code=404)
     return last_replay
